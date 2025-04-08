693753a5285d7321b1d728db8c1801d14ef23ff0
Amritha <amrithak327@gmail.com>
2025-04-08 22:32:50 +0530

    Routing in components

--- book_management_system/src/App.jsx
+++ book_management_system/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
   return (
     <div>
-      <Login/>
-      <Register/>
+      <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
