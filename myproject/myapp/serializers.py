0c35df5844de0a96a73b5203c6ffedbf10402a1c
ssupkunjon <najeebmuhammed030@gmail.com>
2025-04-16 11:11:34 +0530

    Implement Signup API with Password Hashing

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -9,17 +9,17 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['email', 'first_name', 'last_name', 'password']
 
     def validate_email(self, value):
-        """
-        Check if the email is already in use.
-        """
+        
+        # Check if the email is already in use.
+        
         if StudentUser.objects.filter(email=value).exists():
             raise serializers.ValidationError("This email is already in use.")
         return value
 
     def create(self, validated_data):
-        """
-        Create and return a new user instance with hashed password.
-        """
+        
+        # Create and return a new user instance with hashed password.
+        
         password = validated_data.pop('password')
         user = StudentUser.objects.create(**validated_data)
         user.set_password(password)  # Hash the password before saving
